7e2717f00fe295dccb7daa378b38a9b4b27af8bf
Stefan Borghesani <stefan.borghesani@gmail.com>
2021-12-16 15:51:25 -0600

    changed quote text from charfield to textfield

--- quotewallapi/models/quote.py
+++ quotewallapi/models/quote.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Quote(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     group = models.ForeignKey("Group", on_delete=models.CASCADE, related_name='groups')
-    quote_text = models.CharField(max_length=500)
+    quote_text = models.TextField()
     quoter = models.CharField(max_length=75)
-    context = models.CharField(max_length=500)
+    context = models.TextField()
     date_added = models.DateField(auto_now=False, auto_now_add=False)
